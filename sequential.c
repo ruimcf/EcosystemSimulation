04c0868ca69df464c72e392541abb44c161f21f7
ruimcf <ruimcf95@gmail.com>
2017-01-13 23:39:06 +0000

    structs

--- sequential.c
+++ sequential.c
@@ -2,6 +2,13 @@
 #include<stdlib.h>
 #include<string.h>
 
+
+typedef struct object_{
+    char type;
+    int num_gen;
+    int num_food;
+}object;
+
 int GEN_PROC_RABBITS;
 int GEN_PROC_FOXES;
 int GEN_FOOD_FOXES;
@@ -9,13 +16,14 @@ int N_GEN;
 int R;
 int C;
 int N;
-char **world;
+object **world;
+
 
 void print_world() {
   int x,y;
   for(y = 0; y < R; y++) {
     for(x = 0; x < C; x++) {
-      printf("%c ", world[y][x]);
+      printf("%c", world[y][x].type);
     }
     printf("\n");
   }
@@ -23,16 +31,21 @@ void print_world() {
 
 void fill_world(){
   int i, x, y;
-  char object[6], label;
+  char name[6];
   for(i = 0; i < N; i++) {
-    scanf("%s %d %d", object, &x, &y);
-    if(strcmp(object, "RABBIT") == 0)
-      label = 'R';
-    else if(strcmp(object, "FOX") == 0)
-      label = 'F';
-    else if(strcmp(object, "ROCK") == 0)
-      label = '*';
-    world[y][x] = label;
+    scanf("%s %d %d", name, &x, &y);
+    object new_addition;
+    if(strcmp(name, "RABBIT") == 0){
+      new_addition.type='R';
+      new_addition.num_gen=GEN_PROC_RABBITS;
+    }else if(strcmp(name, "FOX") == 0){
+      new_addition.type='F';
+      new_addition.num_gen=GEN_PROC_FOXES;
+      new_addition.num_food=GEN_FOOD_FOXES;
+    }else if(strcmp(name, "ROCK") == 0){
+      new_addition.type='*';
+    }
+    world[x][y] = new_addition;
   }
 }
 
@@ -44,10 +57,10 @@ int main() {
   scanf("%d", &R);
   scanf("%d", &C);
   scanf("%d", &N);
-  world = (char **)malloc(sizeof(char*) * R);
+  world = (object **)malloc(sizeof(object*) * R);
   int i;
   for(i = 0; i < R; i++) {
-    world[i] = (char *)calloc(C, sizeof(char));
+    world[i] = (object *)malloc(C * sizeof(object));
   }
   fill_world();
   print_world();
